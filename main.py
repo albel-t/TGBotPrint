c52b722ac3d201ba65d3a41a1a8a902c1b2a0b06
albel260819 <albel260819@gmail.com>
2025-06-07 18:18:09 +0600

    набросок

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import debug
 import chat_logger_bot
 import short_test
+import subjection
 
-
-import sys
-import threading
 from PyQt6.QtWidgets import QApplication
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
 
-if __name__ == "__main__":
+translate_to_app = False
 
+API_TOKEN = subjection.bot_token
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
 
+@dp.message(Command("start"))
+async def run_external_function(message: types.Message):
+    global translate_to_app
+    # запуск приложения
+    translate_to_app = True
+    await message.answer("запущена трансляция")     
 
-    chat_logger_bot.main()
+@dp.message()
+async def echo(message: types.Message):
+    global translate_to_app
+    if translate_to_app:
+        #вывод в приложение
+    else:
+        await message.answer(message.text)
 
-    # Основной поток продолжает работать
-    print("Основной поток бота продолжает работу")
-    print("Поток бота завершил работу")
-    
+if __name__ == '__main__':
+    dp.run_polling(bot)
